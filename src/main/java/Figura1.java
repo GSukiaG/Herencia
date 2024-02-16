9a7c831d3a47f896931a6c1626d8168661be49d0
GSukia <garbine.sukia@ifp.es>
2024-02-16 14:20:27 +0100

    añadidas clases
    He añadido

---
+++ src/main/java/Figura1.java
@@ -0,0 +1,22 @@
+/**
+ *
+ * @author Garbine
+ */
+    public  class Figura1 {
+    private String color;
+
+    public Figura1(String color)
+    {
+        this.color = color;
+    }
+
+
+    public String getColor()
+    {
+        return color;
+    }
+}
+    
+
+
+
